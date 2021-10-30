3997b7f825fb510eed31fecca2421fe9ab557a2d
turtlecode <85156399+turtlecode@users.noreply.github.com>
2021-10-30 22:55:22 +0300

    How to use colorama libary in Python?
    Do you know that we can color the text we print to the console using the colorama library in the Python programming language? Using the 'colorama' library in the Python programming language, we can change the colors of the texts we print to the console with the 'print' command. In this video, we will color the texts we print on the console screen using the colorama library in the python programming language.
    
    You can support us by subscribing to Turtle Code Youtube channel and following other social media accounts.
    
    Subscribe to Youtube Channel
    ➤ https://l24.im/2L9hbGr
    GitHub - Source Codes
    ➤ https://l24.im/zDgj7yi

---
+++ Print Colored Text - Python.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi! My name is Turtle Code" + Fore.YELLOW+Back.YELLOW+"I am Machine Learning Instructor")
+print(Back.CYAN+"Hi! My name is Turtle Code")
+print(Fore.RED+Back.GREEN+"Hi! My name is Turtle Code")
